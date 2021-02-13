4ec11cf3ccdca6c78a1d6ab0ffa13fbc64747a48
keirbailey <keirinvestment@gmail.com>
2021-02-13 16:16:14 +0000

    Removed unrequired debug code

--- main.go
+++ main.go
@@ -24,11 +24,6 @@ func run() error {
 	if err != nil {
 		log.Fatal(fmt.Sprintf("failed to init environment config: %s", err))
 	}
-	print(cfg)
-
-
-	myMap := make(map[string]float32)
-	fmt.Printf("%v", myMap)
 
 	//traderService, err := service.NewTradingEngine(&cfg.Engine, log)
 	//if err != nil {
