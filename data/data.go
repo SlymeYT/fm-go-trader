d740714ff461bb49c448a85e0a004802584c7b8e
keirbailey <keirinvestment@gmail.com>
2021-02-13 11:55:11 +0000

    Added data related structs and interface

---
+++ data/data.go
@@ -0,0 +1,48 @@
+package data
+
+import (
+	"github.com/sheerun/queue"
+	"go.uber.org/zap"
+	"time"
+)
+
+const(
+	dataDirectory = "data/"
+	timestampLayoutIso = "2006-01-02"
+)
+
+type Handler interface {
+	ShouldContinue() bool
+	UpdateData() error
+	GetLatestData() (*symbolData, int64)
+}
+
+type Config struct {
+	Log             *zap.Logger
+	EventQ          *queue.Queue
+	Ticker          string
+	Timeframe       string
+	Exchange        string
+}
+
+type simulatedHandler struct {
+	log               *zap.Logger	// Pointer to universal logger
+	eventQ            *queue.Queue	// Pointer to the trader pair's event queue
+	symbol            string     	// symbol the data is representing
+	allSymbolData     symbolData 	// All the data available from historic data file
+	currentSymbolData symbolData 	// Data available up to current timestamp
+	latestBarIndex    int64      	// Current index of the latest bar in the symbolData
+}
+
+type symbolData struct {
+	Timestamps 	[]time.Time
+	Opens 		[]float64
+	Highs 		[]float64
+	Lows 		[]float64
+	Closes 		[]float64
+	Volumes 	[]uint64
+	Indicators 	map[string][]interface{}
+}
+
+
+
--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1
+	github.com/sheerun/queue v1.0.1
 	go.uber.org/zap v1.16.0
 )
