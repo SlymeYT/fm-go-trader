7ccd0d0c4453f958c0f7d79ee99da8c7c6dfe4a4
keirbailey <keirinvestment@gmail.com>
2021-02-13 16:14:42 +0000

    Added basic order and fill event struct models

--- model/event.go
+++ model/event.go
@@ -11,3 +11,34 @@ type SignalEvent struct {
 	Symbol 		string
 	SignalPairs map[string]float32 	// map[direction]strength
 }
+
+type OrderEvent struct {
+	Timestamp 	time.Time
+	Symbol    	string
+	Quantity   		float64
+	Direction  		string
+}
+
+type FillEvent struct {
+	Timestamp  		time.Time
+	Symbol     		string
+	Exchange   		string
+	Quantity   		float64
+	Direction  		string
+	FillCost   		float64
+	CommissionFee 	float64
+	ExchangeFee		float64
+	//IsExit 			bool
+}
+
+func (f *FillEvent) CalculateCommissionFee() float64 {
+	return 0.0
+}
+
+func (f *FillEvent) CalculateExchangeFee() float64 {
+	return 0.0
+}
+
+func (f *FillEvent) CalculateFillCost() float64 {
+	return 0.0
+}
