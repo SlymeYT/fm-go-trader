09677a4e4bcfe1e9c64ddee09108daf7a84ef6d1
keirbailey <keirinvestment@gmail.com>
2021-02-13 16:15:36 +0000

    Added execution impl with GenerateFills() method

--- execution/execution.go
+++ execution/execution.go
@@ -1,1 +1,48 @@
 package execution
+
+import (
+	"github.com/sheerun/queue"
+	"gitlab.com/open-source-keir/financial-modelling/trading/fm-trader/config"
+	"gitlab.com/open-source-keir/financial-modelling/trading/fm-trader/model"
+	"go.uber.org/zap"
+	"time"
+)
+
+type Execution interface {
+	GenerateFills(model.OrderEvent) error
+}
+
+type simulatedExecution struct {
+	log      *zap.Logger
+	eventQ   *queue.Queue
+	exchange string
+}
+
+// GenerateFills takes an OrderEvent, executes it, and produces a FillEvent that is appended to the event queue
+func (se *simulatedExecution) GenerateFills(order model.OrderEvent) error {
+	// Todo: Add latency, slippage, etc
+
+	// Assume all orders are filled at the market price
+	fill := model.FillEvent{
+		Timestamp: time.Now(),
+		Symbol:    order.Symbol,
+		Exchange:  se.exchange,
+		Quantity:  order.Quantity,
+		Direction: order.Direction,
+	}
+	fill.CommissionFee = fill.CalculateCommissionFee() 	// 0.0
+	fill.ExchangeFee = fill.CalculateExchangeFee() 		// 0.0
+	fill.FillCost = fill.CalculateFillCost() 			// 0.0
+
+	se.eventQ.Append(fill)
+	return nil
+}
+
+// NewSimulatedExecution constructs an Execution instance
+func NewSimulatedExecution(cfg config.Trader, logger *zap.Logger, eventQ *queue.Queue) *simulatedExecution {
+	return &simulatedExecution{
+		log:      logger,
+		eventQ:   eventQ,
+		exchange: cfg.Exchange,
+	}
+}
