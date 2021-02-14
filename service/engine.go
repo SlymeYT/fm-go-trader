169a961a673731185df6ea855acd055bf4ab309b
keirbailey <keirinvestment@gmail.com>
2021-02-14 15:58:13 +0000

    added engine constructor and trader config generation

--- service/engine.go
+++ service/engine.go
@@ -1,12 +1,15 @@
 package service
 
 import (
+	"fmt"
+	"github.com/pkg/errors"
+	"gitlab.com/open-source-keir/financial-modelling/trading/fm-trader/config"
 	"gitlab.com/open-source-keir/financial-modelling/trading/fm-trader/trader"
 	"go.uber.org/zap"
 )
 
 type TradingEngine interface {
-	RunBacktest()
+	RunBacktest() error
 	RunTraderLive()
 	RunTraderDry()
 }
@@ -16,14 +19,69 @@ type tradingEngine struct {
 	traders []trader.Trader
 }
 
-func (t *tradingEngine) RunBacktest() {
-	// Run backtest of meta-strategy for specified trading pairs (on go routine with unique id that can be cancelled)
-	// 1. Parse meta-strategy from static config or http request
-	// 2. Parse symbols to trade from static config or http request
-	// 3. Spin up pair Traders on go routines
-	// 4. Report results
-
+func (t *tradingEngine) RunBacktest() error {
 	for _, traderPair := range t.traders {
-		traderPair.Run()
+		if err := traderPair.Run(); err != nil {
+			return errors.Wrap(err, "failed to RunBacktest()")
+		}
 	}
+	return nil
+}
+
+func (t *tradingEngine) RunTraderLive() error {
+	// Run live trading with meta-strategy for specified trading pairs
+	return nil
+}
+
+func (t *tradingEngine) RunTraderDry() error {
+	// Run live trading with meta-strategy for specified trading pairs
+	return nil
+}
+
+func NewTradingEngine(cfg *config.Engine, log *zap.Logger) (*tradingEngine, error) {
+	traders, err := buildTraders(cfg, log)
+	if err != nil {
+		return &tradingEngine{}, err
+	}
+
+	engine := &tradingEngine{
+		log:     log,
+		traders: traders,
+	}
+
+	return engine, nil
+}
+
+func buildTraders(cfg *config.Engine, log *zap.Logger) ([]trader.Trader, error) {
+	var traders []trader.Trader
+	for index, cfg := range buildTraderConfigs(cfg, log) {
+		traderPair, err := trader.NewTrader(cfg)
+		if err != nil {
+			return traders, errors.Wrap(err, fmt.Sprintf("failed to init trader %v with config: %+v\n", index, cfg))
+		}
+		traders = append(traders, traderPair)
+	}
+	return traders, nil
+}
+
+func buildTraderConfigs(cfg *config.Engine, log *zap.Logger) []config.Trader {
+	// Todo: Remove these stubs with the real array from the config.Engine struct
+	tickers := []string{cfg.Symbols}
+	timeframes := []string{cfg.Timeframes}
+	exchanges := []string{cfg.Exchanges}
+	startingCash := cfg.StartingCash / float64(len(tickers))
+	defaultOrderValue := startingCash / 10
+
+	var traderConfigs []config.Trader
+	for index, symbol := range tickers {
+		traderConfigs = append(traderConfigs, config.Trader{
+			Log: 				log,
+			Symbol:    			symbol,
+			Timeframe:      	timeframes[index],
+			Exchange:       	exchanges[index],
+			StartingCash: 		startingCash,
+			DefaultOrderValue: 	defaultOrderValue,
+		})
+	}
+	return traderConfigs
 }
